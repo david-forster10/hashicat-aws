eab2f5827a78a6442f7f9358a8b4b16ea574295f
David Forster <dforster10@hotmail.com>
2024-07-18 09:51:19 +0000

    Added the second tag

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     organization = "hcp-training-dforster"
     workspaces {
       name = "hashicat-aws"
